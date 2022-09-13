063fec9ffe48bd2f69f06ab0c140d0dcc5b0967d
Leo Vergara <leoivs@hotmail.com>
2022-09-13 00:54:35 -0300

    [Leo Vergara]-Inicialización del proyecto

---
+++ src/main/java/com/app/proyecto1tingeso/Proyecto1TingesoApplication.java
@@ -0,0 +1,13 @@
+package com.app.proyecto1tingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1TingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1TingesoApplication.class, args);
+	}
+
+}
