7484c18fb56ee9052729329dbfdef5e680e0f9fc
Leo Vergara <leoivs@hotmail.com>
2022-09-13 01:29:03 -0300

    [Leo Vergara]-Servicios empleado

--- src/main/java/com/app/proyecto1tingeso/repositories/EmpleadoRepository.java
+++ src/main/java/com/app/proyecto1tingeso/repositories/EmpleadoRepository.java
@@ -2,7 +2,9 @@ package com.app.proyecto1tingeso.repositories;
 
 import com.app.proyecto1tingeso.entities.EmpleadoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmpleadoRepository extends JpaRepository<EmpleadoEntity, Long> {
     public EmpleadoEntity findByRut(String rut);
 }
