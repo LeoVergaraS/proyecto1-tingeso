bd3c410f4eaaeefaa318ac6c6a584ee83dd49538
Leo Vergara <leoivs@hotmail.com>
2022-09-22 00:14:02 -0300

    [Leo Vergara]-Servicio de sueldo

---
+++ src/main/java/com/app/proyecto1tingeso/services/SueldoService.java
@@ -0,0 +1,20 @@
+package com.app.proyecto1tingeso.services;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.proyecto1tingeso.entities.SueldoEntity;
+import com.app.proyecto1tingeso.repositories.SueldoRepository;
+
+@Service
+public class SueldoService {
+
+    @Autowired
+    SueldoRepository sueldoRepository;
+
+    public ArrayList<SueldoEntity> obtenerSueldos(){
+        return (ArrayList<SueldoEntity>) sueldoRepository.findAll();
+    }
+}
