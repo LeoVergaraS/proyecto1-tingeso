b8475b30651d40d2f2373ae940f499e68391767c
Leo Vergara <leoivs@hotmail.com>
2022-09-28 21:00:39 -0300

    [Leo Vergara]-Repositorio atrasos

---
+++ src/main/java/com/app/proyecto1tingeso/repositories/AtrasosRepository.java
@@ -0,0 +1,9 @@
+package com.app.proyecto1tingeso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.proyecto1tingeso.entities.AtrasosEntity;
+
+public interface AtrasosRepository extends JpaRepository<AtrasosEntity, Long>{
+    
+}
